710c066c31e3dedd73a43d7ccb49d0878bf3f368
Henrik W. Moe <henrik.walker.moe@bekk.no>
2020-11-05 20:46:49 +0100

    Adds solution and project

---
+++ src/AnonymousTokens/AnonymousTokensConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AnonymousTokensConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
