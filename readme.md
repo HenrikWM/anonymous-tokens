a33248bbc72af72e620b58f314e24b399d8b4dbd
Henrik W. Moe <henrik.walker.moe@bekk.no>
2020-11-05 21:22:01 +0100

    Implement the Elliptic Curve keypair generator

--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 # anonymous-tokens
 
 A C#-implementation of https://github.com/tjesi/anonymous-tokens.
+
+Sources:
+
+- [Elliptical curve cryptography with Bouncy Castle](https://www.codeproject.com/Tips/1150485/Csharp-Elliptical-Curve-Cryptography-with-Bouncy-C)
--- src/AnonymousTokens/AnonymousTokensConsole/Program.cs
+++ src/AnonymousTokens/AnonymousTokensConsole/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Security;
 
 namespace AnonymousTokensConsole
 {
@@ -6,7 +7,25 @@ namespace AnonymousTokensConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var strength = 256;
+
+            // Generate private key k,
+            // and public key K.
+            var generator = new ECKeyPairGenerator("EC");
+            generator.Init(new KeyGenerationParameters(new SecureRandom(), strength));
+            var keyPair = generator.GenerateKeyPair();
+
+            // Generate token Q = k*P, and create
+            // proof (c,z) of correctness, given G and K.
+            // TODO
+
+            // Randomise the token Q, by removing
+            // the mask r: W = (1/r)*Q = k*P.
+            // Also checks that proof (c,z) is correct.
+            // TODO
+
+            // Verify that the token (t,W) is correct.
+            // TODO            
         }
     }
 }
