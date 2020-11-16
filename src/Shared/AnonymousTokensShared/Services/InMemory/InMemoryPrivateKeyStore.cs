429bd8211f9faa96db46f829468652532c4d8a2e
Henrik W. Moe <henrik.walker.moe@bekk.no>
2020-11-16 22:25:59 +0100

    Try to load inmemory keys with correct form

--- src/Shared/AnonymousTokensShared/Services/InMemory/InMemoryPrivateKeyStore.cs
+++ src/Shared/AnonymousTokensShared/Services/InMemory/InMemoryPrivateKeyStore.cs
@@ -1,7 +1,6 @@
-﻿using Org.BouncyCastle.Crypto.Parameters;
+﻿using Org.BouncyCastle.Asn1.X9;
+using Org.BouncyCastle.Crypto.Parameters;
 using Org.BouncyCastle.Math;
-using Org.BouncyCastle.Math.EC;
-using Org.BouncyCastle.Utilities.Encoders;
 
 namespace AnonymousTokensShared.Services.InMemory
 {
@@ -9,22 +8,11 @@ namespace AnonymousTokensShared.Services.InMemory
     {
         public ECPrivateKeyParameters Get()
         {
-            BigInteger n = new BigInteger("6277101735386680763835789423176059013767194773182842284081");
+            X9ECParameters p = ECNamedCurveTable.GetByName("secp256k1");
 
-            FpCurve curve = new FpCurve(
-                new BigInteger("6277101735386680763835789423207666416083908700390324961279"), // q
-                new BigInteger("fffffffffffffffffffffffffffffffefffffffffffffffc", 16), // a
-                new BigInteger("64210519e59c80e70fa7e9ab72243049feb8deecc146b9b1", 16), // b
-                n, BigInteger.One);
-
-            ECDomainParameters parameters = new ECDomainParameters(
-                curve,
-                curve.DecodePoint(Hex.Decode("03188da80eb03090f67cbf20eb43a18800f4ff0afd82ff1012")), // G
-                n, BigInteger.One);
-
+            ECDomainParameters parameters = new ECDomainParameters(p.Curve, p.G, p.N, p.H);
             ECPrivateKeyParameters privateKeyParameters = new ECPrivateKeyParameters(
-                "EC",
-                new BigInteger("651056770906015076056810763456358567190100156695615665659"), // d
+                new BigInteger("20186677036482506117540275567393538695075300175221296989956723148347484984008"), // d
                 parameters);
 
             return privateKeyParameters;
--- src/Shared/AnonymousTokensShared/Services/InMemory/InMemoryPublicKeyStore.cs
+++ src/Shared/AnonymousTokensShared/Services/InMemory/InMemoryPublicKeyStore.cs
@@ -1,6 +1,5 @@
-﻿using Org.BouncyCastle.Crypto.Parameters;
-using Org.BouncyCastle.Math;
-using Org.BouncyCastle.Math.EC;
+﻿using Org.BouncyCastle.Asn1.X9;
+using Org.BouncyCastle.Crypto.Parameters;
 using Org.BouncyCastle.Utilities.Encoders;
 
 namespace AnonymousTokensShared.Services.InMemory
@@ -9,23 +8,13 @@ namespace AnonymousTokensShared.Services.InMemory
     {
         public ECPublicKeyParameters Get()
         {
-            BigInteger n = new BigInteger("6277101735386680763835789423176059013767194773182842284081");
+            X9ECParameters p = ECNamedCurveTable.GetByName("secp256k1");
 
-            FpCurve curve = new FpCurve(
-                new BigInteger("6277101735386680763835789423207666416083908700390324961279"), // q
-                new BigInteger("fffffffffffffffffffffffffffffffefffffffffffffffc", 16), // a
-                new BigInteger("64210519e59c80e70fa7e9ab72243049feb8deecc146b9b1", 16), // b
-                n, BigInteger.One);
-
-            ECDomainParameters parameters = new ECDomainParameters(
-                curve,
-                curve.DecodePoint(Hex.Decode("03188da80eb03090f67cbf20eb43a18800f4ff0afd82ff1012")), // G
-                n, BigInteger.One);
+            ECDomainParameters parameters = new ECDomainParameters(p.Curve, p.G, p.N, p.H);
 
             ECPublicKeyParameters publicKeyParameters = new ECPublicKeyParameters(
-                "EC",
-                curve.DecodePoint(Hex.Decode("0262b12d60690cdcf330babab6e69763b471f994dd702d16a5")), // Q
-                parameters);
+               parameters.Curve.DecodePoint(Hex.Decode("03596375E6CE57E0F20294FC46BDFCFD19A39F8161B58695B3EC5B3D16427C274D")), // Q
+               parameters);
 
             return publicKeyParameters;
         }
