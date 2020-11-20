2e71033b061cec82da4e7762323c13ce7e62a13c
Tjerand Silde <tjerand.silde@ntnu.no>
2020-11-20 20:50:16 +0100

    XML-comments for ECPointVerifier.PointIsValid()

--- src/AnonymousTokens/ECPointVerifier.cs
+++ src/AnonymousTokens/ECPointVerifier.cs
@@ -4,6 +4,12 @@ namespace AnonymousTokens
 {
     public static class ECPointVerifier
     {
+        /// <summary>
+        /// Verifies that a point is valid, on the correct curve and in the corect subgroup.
+        /// </summary>
+        /// <param name="point">Elliptic curve point that we want to verify</param>
+        /// <param name="curve">Elliptic curce that we want to verify against</param>
+        /// <returns>True if the point is valid and otherwise false</returns>
         public static bool PointIsValid(ECPoint point, ECCurve curve)
         {
             if (point == null || !curve.Equals(point.Curve) || point.Multiply(curve.Cofactor).Equals(curve.Infinity))
