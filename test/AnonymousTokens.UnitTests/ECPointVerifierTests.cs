61a370919661c87500dc84cb8354d00cc79a1315
Henrik W. Moe <henrik.walker.moe@bekk.no>
2020-11-22 20:35:58 +0100

    Extract Fp to file

--- test/AnonymousTokens.UnitTests/ECPointVerifierTests.cs
+++ test/AnonymousTokens.UnitTests/ECPointVerifierTests.cs
@@ -1,7 +1,7 @@
 
+using AnonymousTokens.UnitTests.TestUtilities;
+
 using Org.BouncyCastle.Asn1.X9;
-using Org.BouncyCastle.Math;
-using Org.BouncyCastle.Math.EC;
 
 using Xunit;
 
@@ -54,43 +54,6 @@ namespace AnonymousTokens.UnitTests
 
             // Assert
             Assert.False(actual);
-        }
-
-        /// <summary>
-        /// Test-class from ECPointTest.cs in Org.BouncyCastle.Math.EC.Tests
-        /// </summary>
-        private class Fp
-        {
-            internal static readonly BigInteger q = new BigInteger("29");
-
-            internal static readonly BigInteger a = new BigInteger("4");
-
-            internal static readonly BigInteger b = new BigInteger("20");
-
-            internal static readonly BigInteger n = new BigInteger("38");
-
-            internal static readonly BigInteger h = new BigInteger("1");
-
-            internal static readonly ECCurve curve = new FpCurve(q, a, b, n, h);
-
-            internal static readonly ECPoint infinity = curve.Infinity;
-
-            internal static readonly int[] pointSource = { 5, 22, 16, 27, 13, 6, 14, 6 };
-
-            internal static ECPoint[] p = new ECPoint[pointSource.Length / 2];
-
-            /**
-             * Creates the points on the curve with literature values.
-             */
-            internal static void CreatePoints()
-            {
-                for (int i = 0; i < pointSource.Length / 2; i++)
-                {
-                    p[i] = curve.CreatePoint(
-                        new BigInteger(pointSource[2 * i].ToString()),
-                        new BigInteger(pointSource[2 * i + 1].ToString()));
-                }
-            }
         }
     }
 }
