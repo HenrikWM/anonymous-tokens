313fb3fb985d13f21e5334499584736227f4d1ef
Martin Strand <martin.strand@gmail.com>
2020-11-26 08:32:03 +0100

    Added more tests

--- test/AnonymousTokens.UnitTests/IntegrationTests/ProtocolTests.cs
+++ test/AnonymousTokens.UnitTests/IntegrationTests/ProtocolTests.cs
@@ -4,6 +4,7 @@ using AnonymousTokens.Services.InMemory;
 
 using Org.BouncyCastle.Asn1.X9;
 using Org.BouncyCastle.Crypto.EC;
+using Org.BouncyCastle.Math;
 
 using Xunit;
 
@@ -13,6 +14,7 @@ namespace AnonymousTokens.UnitTests.IntegrationTests
     {
         private Initiator _initiator;
         private TokenGenerator _tokenGenerator;
+        private TokenGenerator _corruptedTokenGenerator;
         private TokenVerifier _tokenVerifier;
         private X9ECParameters _ecParameters;
 
@@ -26,10 +28,12 @@ namespace AnonymousTokens.UnitTests.IntegrationTests
 
             var privateKeyStore = new InMemoryPrivateKeyStore();
             var privateKey = privateKeyStore.Get();
+            var wrongPrivateKey = privateKey.Add(BigInteger.One);
 
             _initiator = new Initiator(publicKey);
 
             _tokenGenerator = new TokenGenerator(publicKey, privateKey);
+            _corruptedTokenGenerator = new TokenGenerator(publicKey, wrongPrivateKey);
 
             _tokenVerifier = new TokenVerifier(privateKey, new InMemorySeedStore());
         }
@@ -85,6 +89,142 @@ namespace AnonymousTokens.UnitTests.IntegrationTests
             {
                 Assert.True(false, "token was replayed and was expected to be not valid");
             }
+        }
+
+        [Fact]
+        public void Run_FailWhenKeysDontMatch()
+        {
+            // 1. Initiate communication with a masked point P = r*T = r*Hash(t)
+            var init = _initiator.Initiate(_ecParameters.Curve);
+            var t = init.t;
+            var r = init.r;
+            var P = init.P;
+
+            // 2'. Generate invalid token and proof
+            var (Q, proofC, proofZ) = _corruptedTokenGenerator.GenerateToken(_ecParameters, P);
+
+            // Verify the proof
+            var isValid = _initiator.VerifyProof(_ecParameters, P, Q, proofC, proofZ);
+            Assert.False(isValid, "Keys were incorrect and the proof did not get verified");
+        }
+
+        [Fact]
+        public void Run_FailWhenTokenIsMalformed()
+        {
+            // 1. Initiate communication with a masked point P = r*T = r*Hash(t)
+            var init = _initiator.Initiate(_ecParameters.Curve);
+            var t = init.t;
+            var r = init.r;
+            var P = init.P;
+
+            // 2'. Generate invalid token and proof
+            var (Q, proofC, proofZ) = _corruptedTokenGenerator.GenerateToken(_ecParameters, P);
+
+            // Change the point Q to something else
+            var changedQ = Q.Twice();
+
+            // Try randomising the token
+            var isValid = _initiator.VerifyProof(_ecParameters, P, changedQ, proofC, proofZ);
+            Assert.False(isValid, "The token was malformed and the proof did not get verified");
+        }
+
+        [Fact]
+        public void Run_FailWhenChallengeIsWrong()
+        {
+            // 1. Initiate communication with a masked point P = r*T = r*Hash(t)
+            var init = _initiator.Initiate(_ecParameters.Curve);
+            var t = init.t;
+            var r = init.r;
+            var P = init.P;
+
+            // 2'. Generate invalid token and proof
+            var (Q, proofC, proofZ) = _corruptedTokenGenerator.GenerateToken(_ecParameters, P);
+
+            // Change the challenge proofC to something else
+            var changedC = proofC.Add(BigInteger.One);
+
+            // Try randomising the token
+            var isValid = _initiator.VerifyProof(_ecParameters, P, Q, changedC, proofZ);
+            Assert.False(isValid, "The challenge was changed and the proof did not get verified");
+        }
+
+        [Fact]
+        public void Run_FailWhenResponseIsWrong()
+        {
+            // 1. Initiate communication with a masked point P = r*T = r*Hash(t)
+            var init = _initiator.Initiate(_ecParameters.Curve);
+            var t = init.t;
+            var r = init.r;
+            var P = init.P;
+
+            // 2'. Generate invalid token and proof
+            var (Q, proofC, proofZ) = _corruptedTokenGenerator.GenerateToken(_ecParameters, P);
+
+            // Change the challenge proofC to something else
+            var changedZ = proofZ.Add(BigInteger.One);
+
+            // Try randomising the token
+            var isValid = _initiator.VerifyProof(_ecParameters, P, Q, proofC, changedZ);
+            Assert.False(isValid, "The response was changed and the proof did not get verified");
+        }
+
+        [Fact]
+        public void Run_FailWhenPIsNotAValidPoint()
+        {
+            // 1. Initiate communication with a masked point P = r*T = r*Hash(t)
+            var init = _initiator.Initiate(_ecParameters.Curve);
+            var t = init.t;
+            var r = init.r;
+            var P = init.P;
+
+            // Create a new point with invalid coordinates
+            var invalidP = _ecParameters.Curve.CreatePoint(P.XCoord.ToBigInteger().Add(BigInteger.One), P.YCoord.ToBigInteger());
+
+            // 2. Generate token Q = k*P and proof (c,z) of correctness
+            try
+            {
+                var (Q, proofC, proofZ) = _tokenGenerator.GenerateToken(_ecParameters, invalidP);
+            }
+            catch (AnonymousTokensException e)
+            {
+                Assert.True(true, "an invalid point should raise an exception");
+            }
+        }
+
+        [Fact]
+        public void Run_FailWhenWIsInvalid()
+        {
+            // 1. Initiate communication with a masked point P = r*T = r*Hash(t)
+            var init = _initiator.Initiate(_ecParameters.Curve);
+            var t = init.t;
+            var r = init.r;
+            var P = init.P;
+
+            // 2. Generate token Q = k*P and proof (c,z) of correctness
+            var (Q, proofC, proofZ) = _tokenGenerator.GenerateToken(_ecParameters, P);
+
+            // 3. Randomise the token Q, by removing the mask r: W = (1/r)*Q = k*T. Also checks that proof (c,z) is correct.
+            var W = _initiator.RandomiseToken(_ecParameters, P, Q, proofC, proofZ, r);
+
+            // Create a new point with invalid coordinates
+            var invalidW = _ecParameters.Curve.CreatePoint(W.XCoord.ToBigInteger().Add(BigInteger.One), W.YCoord.ToBigInteger());
+
+            // Create a new point that is merely inconsistent
+            var wrongW = W.Add(W);
+
+            // 4. Try verifying (t,W).
+
+            try
+            {
+                _tokenVerifier.VerifyToken(_ecParameters.Curve, t, invalidW);
+            }
+            catch (AnonymousTokensException e)
+            {
+                Assert.True(true, "an invalid point should raise an exception");
+            }
+
+            var isVerified = _tokenVerifier.VerifyToken(_ecParameters.Curve, t, wrongW);
+            Assert.False(isVerified, "a wrong point was expected to fail verification");
         }
     }
 }
