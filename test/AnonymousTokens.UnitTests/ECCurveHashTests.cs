f1140ea05949cf46cb2445cd3d1d97f501ba9021
Henrik W. Moe <henrik.walker.moe@bekk.no>
2020-11-29 13:17:34 +0100

    Adds random out of range bytes

--- test/AnonymousTokens.UnitTests/ECCurveHashTests.cs
+++ test/AnonymousTokens.UnitTests/ECCurveHashTests.cs
@@ -1,4 +1,5 @@
 using Org.BouncyCastle.Asn1.X9;
+using Org.BouncyCastle.Math;
 
 using Xunit;
 
@@ -6,13 +7,16 @@ namespace AnonymousTokens.UnitTests
 {
     public class ECCurveHashTests
     {
-        [Fact]
-        public void HashToWeierstrassCurve_tIsNotWithinValidRangeOfP_ReturnsNull()
+        [Theory]
+        [InlineData("3342403536405981729393488334694600415596881826869351677613")]
+        [InlineData("5735822328888155254683894997897571951568553642892029982342")]
+        [InlineData("-15392676890630437317721681665788413720756934076096835125044231120241806601564")]
+        public void HashToWeierstrassCurve_tIsNotWithinRangeOfP_ReturnsNull(string randomBigInteger)
         {
             // Arrange
             var x9 = ECNamedCurveTable.GetByName("prime239v1");
             var curve = x9.Curve;
-            byte[]? t = new byte[32];
+            byte[]? t = new BigInteger(randomBigInteger).ToByteArray();
 
             // Act
             var actual = ECCurveHash.HashToWeierstrassCurve(curve, t);
