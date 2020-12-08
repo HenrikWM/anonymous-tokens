e24381864c4df9d62883cd08fb4a98e96ebe75f9
Henrik W. Moe <henrik.walker.moe@bekk.no>
2020-12-08 13:31:47 +0100

    Replace token with placeholder

--- samples/ClientServer/Client/Client.Console/ApiClients/TokenApi/TokenApiClient.cs
+++ samples/ClientServer/Client/Client.Console/ApiClients/TokenApi/TokenApiClient.cs
@@ -35,7 +35,7 @@ namespace AnonymousTokensConsole.ApiClients.TokenApi
             _client = new HttpClient();
             _client.DefaultRequestHeaders.Add("Accept", "application/json");
             _client.BaseAddress = new Uri("https://localhost:5001/");
-            _client.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IkRBOTg5RTI1QzYxRTBFRUYxNTU1MzA2MUJFREIwNUFGIiwidHlwIjoiYXQrand0In0.eyJuYmYiOjE2MDc0Mjg1MjYsImV4cCI6MTYwNzQzMjEyNiwiaXNzIjoiaHR0cHM6Ly9sb2NhbGhvc3Q6NTAwMSIsImF1ZCI6Imh0dHBzOi8vbG9jYWxob3N0OjUwMDEvcmVzb3VyY2VzIiwiY2xpZW50X2lkIjoidGVzdC1zcGEtY2xpZW50Iiwic3ViIjoiZjQ2MjAyODMtZWI5Yy00NjU4LWE2YTUtMTRkZGZhODRjMTFjIiwiYXV0aF90aW1lIjoxNjA3NDI4NTIzLCJpZHAiOiJpZHBvcnRlbiIsImNvdmlkMTlfc3RhdHVzIjoicG9zaXRpdiIsImNvdmlkMTlfYmxva2VyZXQiOiJmYWxzZSIsImNvdmlkMTlfYW5vbnltb3VzX3Rva2VuIjoiYXZhaWxhYmxlIiwiY292aWQxOV9zbWl0dGVfc3RhcnQiOiIyMDIwLTExLTMwIiwianRpIjoiRTkwMjg4MTAzMjg1M0Y4OTVENENCNzNFMEY2NjFBMTciLCJzaWQiOiIzRkQxMDk4NTg0RTQyMzExNTVGRDc5NUVBQkM0N0I3RiIsImlhdCI6MTYwNzQyODUyNiwic2NvcGUiOlsib3BlbmlkIiwic21pdHRlc3RvcCJdLCJhbXIiOlsiZXh0ZXJuYWwiXX0.nX-zwIHNo7zgh_SVnjTfZq57w-6WS8bQfdBS4z4z60NBGntIRUf8OnpSTsVJCtRCy9WgI_9kir8ZygBzktYAJYFgRJNmzN6Yl93nZI8y80t_tmtEGpBq8jQbu4dkRzUPCNDuNwLh4LUjXPoEwCTEZodEYBp4ogsBG0PS_j7yDLWlodW05j2-Sns-9iRiNuoF_WLxKS0RHmIChR3MaLKA4PXZunC4RJTi3EinbUy3nMhhvAtbo3iOLpRlBVXqChzofjr8xbt1IqbF6sXzQaeUJObHU898knEONSQSmm4Xtr8-2O_8FrM6ewpPBNP4kYL__LhaFWsyFTSdBzKeEUpcUw");
+            _client.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", "Bearer <insert access token>");
 
             var result = await _client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
             if (result.IsSuccessStatusCode)
